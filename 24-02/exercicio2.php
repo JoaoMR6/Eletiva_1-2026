50760f11e5e2346c6b1bcafc9dae84fb3a7ad3c1
joao <joaomagalhaesribeiro16@gmail.com>
2026-02-24 21:44:22 -0300

    arrumando o h1 kkkk

--- 24-02/exercicio2.php
+++ 24-02/exercicio2.php
@@ -8,7 +8,7 @@
 </head>
 <body> 
 <div class="container py-3">
-<h1>Exercicio 1</h1>
+<h1>Exercicio 2</h1>
 <form method="post">
 <div class="mb-3">
               <label for="pri_numero" class="form-label">Primeiro-numero</label>
