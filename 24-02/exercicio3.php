69ec27fb767a4e4c6d1e54c0de5cd22175be0811
joao <joaomagalhaesribeiro16@gmail.com>
2026-02-24 21:42:42 -0300

    exercicio 3 e arrumando o 2

--- 24-02/exercicio3.php
+++ 24-02/exercicio3.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercicio 2</title>
+<title>Exercicio 3</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
@@ -23,8 +23,8 @@
     if($_SERVER['REQUEST_METHOD'] =="POST"){
         $valor1=$_POST['pri_numero'];
         $valor2= $_POST['seg_numero'];
-        $sub= $valor1 - $valor2;
-        echo "a subtração é: $sub";
+        $mult= $valor1 * $valor2;
+        echo "a multiplicação é: $mult";
     }
     ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
