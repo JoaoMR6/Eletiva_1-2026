6a0bf716b78d1f72ea63afd249370e7e8894f432
joao.name <joaomagalhaesribeiro16@gmail.com>
2026-03-16 21:06:37 -0300

    finalizei o ex 16 e arrumei o 15

--- 24-02/ex16.php
+++ 24-02/ex16.php
@@ -3,28 +3,29 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercicio 15</title>
+<title>Exercicio 16</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container py-3">
-<h1>Exercicio 15</h1>
+<h1>Exercicio 16</h1>
 <form method="post">
 <div class="mb-3">
-              <label for="pri_numero" class="form-label" step="any" >Peso (KG)</label>
-              <input type="number" id="pri_numero" name="pri_numero" class="form-control" required="" step="0.01">
+              <label for="pri_numero" class="form-label">Preço</label>
+              <input type="number" id="pri_numero" name="pri_numero" class="form-control" required="">
             </div><div class="mb-3">
-              <label for="seg_numero" class="form-label" step= "any">Altura (Metros)</label>
-              <input type="number" id="seg_numero" name="seg_numero" class="form-control" required="" step="0.01" >
+              <label for="seg_numero" class="form-label">Desconto em %</label>
+              <input type="number" id="seg_numero" name="seg_numero" class="form-control" required="">
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
 <?php
     if($_SERVER['REQUEST_METHOD'] =="POST"){
-        $peso=$_POST['pri_numero'];
-        $altu= $_POST['seg_numero'];
-        $resul= $peso / ( $altu ** 2) ;
-        echo "IMC = $resul";
+        $preco=$_POST['pri_numero'];
+        $des= $_POST['seg_numero'];
+        $por= $des /100;
+        $resul= $preco - ( $preco * $por) ;
+        echo "$preco - $des% = $resul";
     }
     ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
