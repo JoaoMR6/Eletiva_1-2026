2d5d961805ccac7c127601a10536c5c5dcbdc642
joao.name <joaomagalhaesribeiro16@gmail.com>
2026-03-22 19:13:37 -0300

    ex4 feito

---
+++ lista3/ex4.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 4</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 4</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="pri" class="form-label">primeiro valor</label>
+              <input type="number" id="pri" name="pri" class="form-control" required="">
+            </div><div class="mb-3">
+              
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php 
+    if($_SERVER['REQUEST_METHOD']== "POST"){
+        $valor1= $_POST["pri"];
+        
+
+        if($valor1 > 100){
+            $resul= ($valor1 * 0.15) + $valor1;
+            echo"<p>Valor acima de 100</p>";
+            echo "<p>Novo valor = $resul</p>";
+        }
+
+        else{
+            echo "Valor abaixo ou igual a 100. Portanto não precisa de aumento ";
+        }
+
+    
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
