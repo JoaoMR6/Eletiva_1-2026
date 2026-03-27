ec5ad8b694e70e889307fabc27d665b67bd77925
joao.name <joaomagalhaesribeiro16@gmail.com>
2026-03-26 21:50:55 -0300

    ex 1 finalizado da lista 4 agr sim lkkk

--- lista4/ex1.php
+++ lista4/ex1.php
@@ -3,53 +3,35 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício Contatos</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<title>Exercício 1</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container py-3">
-<h1>exercicio 1</h1>
+<h1>Exercício 1</h1>
 <form method="post">
-<?php for($i = 0; $i < 5; $i++): ?>
-    <div class="mb-3">
-        <label class="form-label">Nome</label>
-        <input type="text" name="nome[]" class="form-control" required>
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Telefone</label>
-        <input type="text" name="telefone[]" class="form-control" required>
-    </div>
-<?php endfor; ?>
+<div class="mb-3">
+              <label for="pri" class="form-label">Informe sua palavra</label>
+              <input type="text" id="pri" name="pri" class="form-control" required="">
+            </div><div class="mb-3">
+              
+            </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
-<?php  
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $contatos= [];
-        
-        for($i= 0; $i<5; $i++){
-            $nome= $_POST["nome"][$i];
-            $telefone= $_POST["telefone"][$i];
+<?php 
+    if($_SERVER['REQUEST_METHOD']== "POST"){
+        $palavra= $_POST["pri"];
 
-            // adicionar no contatos
-            $contatos[$nome]= $telefone;
-            }
+        $resul= mb_strlen($palavra, 'UTF-8');
 
-        //ordernar por nome
-        ksort($contatos);
+        echo"<h1>Resultado </h1>";
+        echo"<p> $resul</p>";
 
-        //exibir contatos
-        
-
-        foreach($contatos as $nome => $telefone){
-                echo "<p>$nome --> $telefone </p>";
-            }
-        
-            
-        }
+       
     
+    }
 ?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
-<div>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
 </body>
 </html>
