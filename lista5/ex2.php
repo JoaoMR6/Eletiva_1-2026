d67528d9c09ec418f0449aeac7102ede5e8d941b
joao.name <joaomagalhaesribeiro16@gmail.com>
2026-04-03 15:59:35 -0300

    ex1 lista 1 sendo feito

--- lista5/ex2.php
+++ lista5/ex2.php
@@ -3,12 +3,12 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício Contatos</title>
+<title>Exercício 2</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body> 
 <div class="container py-3">
-<h1>exercicio 1</h1>
+<h1>exercicio 2</h1>
 <form method="post">
 <?php for($i = 0; $i < 5; $i++): ?>
     <div class="mb-3">
